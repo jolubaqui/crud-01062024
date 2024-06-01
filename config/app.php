3cb08124f53790956272547f3d0a93dec17aae4c
jolubaqui <jolubaqui@gmail.com>
2024-06-01 13:38:51 -0500

    add archivos de configuracion

---
+++ config/app.php
@@ -0,0 +1,5 @@
+<?php
+const APP_URL = "http://localhost/crud-01062024/";
+const APP_NAME = "CRUD POO MYSQL";
+const APP_SESSION_NAME = "CRUD";
+date_default_timezone_set("America/Bogota");
---
+++ config/server.php
@@ -0,0 +1,5 @@
+< <?php
+    const DB_SERVER = "localhost";
+    const DB_NAME = "crud-01062024";
+    const DB_USER = "root";
+    const DB_PSS = "";
